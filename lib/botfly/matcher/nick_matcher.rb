5d5d976531ae6c083f8dcc7f90a2a3033257c67e
Ryan Neufeld <Ryan.Neufeld@CanadaDrugs.com>
2010-03-13 00:52:31 -0600

    Whoops !

--- lib/botfly/matcher/nick_matcher.rb
+++ lib/botfly/matcher/nick_matcher.rb
@@ -3,6 +3,7 @@ require 'botfly/matcher'
 module Botfly
   class NickMatcher < Matcher
     def match(params)
+      Botfly.logger.warn "Matching #{@condition.inspect} against #{params[:nick]}"
       return params[:nick] =~ @condition
     end
   end
