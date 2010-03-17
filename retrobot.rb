5f285c05a9472a49d377ab52b9f6d850da78d8ca
Ryan Neufeld <Ryan.Neufeld@CanadaDrugs.com>
2010-03-17 06:11:24 -0500

    Some additions to retrobot example

--- retrobot.rb
+++ retrobot.rb
@@ -7,7 +7,7 @@ config = YAML::load(ARGF.read) if ARGF
 
 bot = Botfly.login(config["jid"],config["pass"]) do
   join('lazer').as('retro-bot') do
-    on.message.from(/^rkneufeld/).body(/start count/) do
+    on.message.from(/^rkneufeld/).body(/^rb count/) do
       say "All votes are now being tallied. You have three votes, use them wisely"
       say "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
       room[:votes] = []
@@ -15,7 +15,7 @@ bot = Botfly.login(config["jid"],config["pass"]) do
         @body.scan(/(\d+)+/).map {|m| room[:votes] << m.first.to_i }
       end
     end
-    on.message.from(/^rkneufeld/).body(/stop count/) do
+    on.message.from(/^rkneufeld/).body(/^rb stop count/) do
       say "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
       say "Allright, voting is finished"
       remove room[:votes_responder]
@@ -24,6 +24,18 @@ bot = Botfly.login(config["jid"],config["pass"]) do
         h[n] += 1
         h 
       end.sort {|a,b| b[-1] <=> a[-1] }.each {|pair| say "#{pair[0]} => #{pair[-1]}" }
+    end
+    on.message.from(/^rkneufeld/).body(/^rb tally$/) do
+      room[:tally_responders] = []
+      room[:plus] = {}; room[:minus] = {}; room[:delta] = {}
+      room[:tally_responders] << on.message.body(/^+(.*)/) { room[:plus][from]  ||= []; room[:plus][from] << match }
+      room[:tally_responders] << on.message.body(/^∂(.*)/) { room[:delta][from] ||= []; room[:delta][from] << match }
+      room[:tally_responders] << on.message.body(/^-(.*)/) { room[:minus][from] ||= []; room[:minus][from] << match }      
+    end
+    on.message.from(/^rkneufeld/).body(/^rb stop tally/) do
+      # remove responders
+      # tally messages
+      # output with votable format
     end
   end
 end
