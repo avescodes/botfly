a2f44c3d889eee7d03e55d444f699b49d1be581f
Ryan Neufeld <Ryan.Neufeld@CanadaDrugs.com>
2010-04-01 23:26:57 -0500

    Slight refactor of let(:..) location, muc_client#as specs

--- spec/botfly/muc_client_spec.rb
+++ spec/botfly/muc_client_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 include Botfly
 describe Botfly::MUCClient do
+  let(:jid) { stub("JID", :domain => 'domain.com', :node => 'resource') }
+  let(:bot) { stub("Bot", :client => stub_jabber_client, :jid => jid) }
+  let(:muc_client) { MUCClient.new("Room", bot) { } }
   context "initializer" do
-    let(:jid) { stub("JID", :domain => 'domain.com', :node => 'resource') }
-    let(:bot) { stub("Bot", :client => stub_jabber_client, :jid => jid) }
-    subject { MUCClient.new("Room", bot) { } }
+    subject { muc_client }
 
     [:bot, :client, :room, :domain, :resource].each do |ivar|
       it { should assign ivar }
@@ -18,7 +19,9 @@ describe Botfly::MUCClient do
   end
 
   describe "#room" do
-    it "should expose access to @block_state"
+    it "should expose access to @block_state" do
+      muc_client.room.should == muc_client.instance_variable_get("@block_state")
+    end
   end
 
   describe "#execute" do
@@ -35,9 +38,15 @@ describe Botfly::MUCClient do
     it "should complete all steps in order"
   end
 
-  describe "#as" do 
-    it "should assign a resource"
-    it "should execute if given a block"
+  describe "#as" do
+    subject { muc_client }
+    it "should assign resource to given" do
+      muc_client.as(:the_boss)
+      muc_client.resource.should be :the_boss
+    end
+    it "should execute if given a block" do
+      expect { muc_client.as(:foo) { raise "Ran" }}.to raise_error "Ran"
+    end
   end
 
   describe "#leave_room" do
