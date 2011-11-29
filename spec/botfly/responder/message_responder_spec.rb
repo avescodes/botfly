89f1c83f337b8f602d535caa9f84baea6dce2266
Ryan Neufeld <ryan@ryanneufeld.ca>
2011-11-29 09:22:12 -0600

    Just commiting some old code

--- spec/botfly/responder/message_responder_spec.rb
+++ spec/botfly/responder/message_responder_spec.rb
@@ -2,5 +2,18 @@ require 'spec_helper'
 
 include Botfly
 describe Botfly::MessageResponder do
-  it "should be tested"
+  let(:bot) { mock "bot" }
+  subject { MessageResponder.new(bot) }
+  it { should respond_to :reply }
+  it "should pass along reply to the bot" do
+    class << self
+      def from
+        :foo
+      end
+    end
+    msg = mock("msg")
+    Jabber::Message.should_receive(:new).with(:foo,:bar).and_return(msg)
+    bot.should_receive(:send).with(msg)
+    bot.sreply(:baz)
+  end
 end
